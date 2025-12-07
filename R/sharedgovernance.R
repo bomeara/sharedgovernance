b0dfc35b8b6ccccd3b08534ee57ad3ea810afe12
Brian O'Meara <omeara.brian@gmail.com>
2025-12-07 12:39:23 -0500

    cleaning up documentation

--- R/sharedgovernance.R
+++ R/sharedgovernance.R
@@ -1,19 +1,19 @@
-#' Completions
-#' This uses information originally from tables C2024_A, C2023_A, etc. from IPEDS (i.e, from https://nces.ed.gov/ipeds/datacenter/DataFiles.aspx?gotoReportId=7&fromIpeds=true&sid=7585cccc-2e95-4be1-8f9e-29d50fc8796f&rtid=7)
-#' It was processed by code in college_tables to organize it and convert the CIPS codes to names of fields.
+#' Dataset: completions
+#' This uses information originally from tables C2024_A, C2023_A, etc. from IPEDS.
+#' It was processed by code in collegetables.info to organize it and convert the CIPS codes to names of fields.
 #' 
 #' @format
 #' A data.frame with many rows and a few columns. UNITID is the institution ID used to link data.
 "completions_program"
 
-#' Comparison table
+#' Dataset: comparison table
 #' This uses many tables from IPEDS to provide information about colleges over the last ten years. 
 #' 
 #' @format
 #' A data.frame. Data for each college for one year is one row.
 "comparison_table"
 
-#' Populaton by state by age
+#' Dataset: population by state by age
 #' 
 #' This uses US Census data for state populations, currently from 2022: https://www2.census.gov/programs-surveys/popest/technical-documentation/file-layouts/2020-2022/sc-est2022-alldata6.pdf 
 #' 
@@ -21,7 +21,7 @@
 #' It is a tibble with columns for State_FIPS, State, Age, and Population
 "population_by_state_by_age"
 
-#' College chosen comparison table
+#' Dataset: college chosen comparison table
 #' 
 #' This uses information from NCES to indicate for each college which ones it points to as comparisons, which ones point to it for comparisons (might not be the same), and which ones NCES considers the comparison pool
 #' 
@@ -29,7 +29,7 @@
 #' A data.frame
 "college_chosen_comparison_table"
 
-#' Scorecard field organized
+#' Dataset: college scorecard data
 #'
 #' This is information from college scorecard on earnings by institution by field by degree (and sometimes by gender)
 #'
@@ -37,7 +37,7 @@
 #' A data.frame
 "scorecard_field_organized"
 
-#' Categories and fields
+#' Dataset: summary caategories and fields
 #' 
 #' This is categories for the fields in the comparison table
 #' @format
@@ -283,8 +283,8 @@ sg_open_collegetables <- function(institution_id) {
 #' @description
 #' This function returns a smaller set of summary data from the comparison_table for the focal institution and its comparisons.
 #' @examples
-#' id <- sg_find_college("University of Massachusetts, Amherst")
-#' summary_data <- sg_return_college_summary(id, categories=c("Basic", "Institutional_Finances", "Enrollment"), focal_only=TRUE)
+#' id <- sg_find_college("University of Massachusetts, Amherst", verbose=FALSE)
+#' summary_data <- sg_return_college_summary(id, categories=c("Basic", "Institution_Finances", "Enrollment"), focal_only=TRUE)
 #' print(summary_data[, c("Year", "Undergrad full time", "Revenue minus expenses")])
 #' 
 sg_return_college_summary <- function(institution_id, categories=c("Basic"), focal_only=FALSE) {
@@ -306,6 +306,9 @@ sg_return_college_summary <- function(institution_id, categories=c("Basic"), foc
 	)
 	fields_to_include <- c()
 	for (category in categories) {
+		if(!(category %in% categories_and_fields$Category)) {
+			stop(paste0("Category '", category, "' not found. See table(categories_and_fields$Category) for options."))
+		}
 		fields_in_category <- categories_and_fields$Field[
 			categories_and_fields$Category == category
 		]
